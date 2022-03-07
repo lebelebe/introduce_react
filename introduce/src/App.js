067ec1987051d08d5e65c01d1220f312779ddf71
lebelebe <jhw9627@gmail.com>
2022-03-07 18:31:55 +0900

    navi

--- introduce/src/App.js
+++ introduce/src/App.js
@@ -2,18 +2,26 @@ import logo from './img/logoname.svg';
 import logoment from './img/navlogoment.svg';
 import appStyle from './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Snsdiv from './Snsdiv'
+import { useState} from 'react'
 
 function App() {
+  const [snsstate, snsUpdate] = useState(false);
+  const snsFL = (t) => {
+    snsUpdate(t)
+    // snsstate값을 수정해라
+  }
+
   return (
-    <nav>
-      <header className={appStyle.hd+" container d-flex"}>
-        <h1 className={appStyle.logoh}>
+    <nav id={appStyle.navi}>
+      <header className={appStyle.hd+" container-lg d-flex"}>
+        <h1 className={appStyle.logoh+" mt-2 mt-lg-0 mt-md-1 col-md-6 col-2"}>
           <a href="/">
             <img src={logoment} className={appStyle.logoment}></img>
             <img src={logo} className={appStyle.logoname}></img>
           </a>
         </h1>
-        <ul className={appStyle.d1ul+" d-flex col-5"}>
+        <ul className={appStyle.d1ul+" d-flex col-md-5 col-8 text-md-end"}>
           <li className="about col-4">
             <a href="/" className="nuriGothicR">나의소개</a>
           </li>
@@ -24,6 +32,10 @@ function App() {
             <a href="/" className="nuriGothicR">면접제안</a>
           </li>
         </ul>
+        <div id={appStyle.utilMenu}>
+          <button onClick={ (e) => { snsFL(!snsstate) } } className={appStyle.utilButton}>SNS</button>
+          { snsstate && <Snsdiv></Snsdiv> }
+        </div>
       </header>
     </nav>
   );
---
+++ introduce/src/Snsdiv.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Snsdiv = () => {
+    return(
+        <div className='text-center'>
+            <a href='/' className='d-block'>Git</a>
+            <a href='/' className='d-block'>Blog</a>
+        </div>
+    )
+};
+
+export default Snsdiv;
